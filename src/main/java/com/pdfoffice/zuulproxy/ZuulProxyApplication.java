7fa38d8fc4bdbe232ce48959694fb33d644ce46b
shubham <shubhamr837@gmail.com>
2019-12-17 11:15:51 +0530

    Added basic files

---
+++ src/main/java/com/pdfoffice/zuulproxy/ZuulProxyApplication.java
@@ -0,0 +1,23 @@
+package com.pdfoffice.zuulproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulProxyApplication {
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulProxyApplication.class, args);
+	}
+
+}
